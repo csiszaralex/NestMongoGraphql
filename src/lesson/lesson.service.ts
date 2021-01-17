ca95e008d229987137466c18a6f1c9670c85f992
Alex <csiszaralex@gmail.com>
2021-01-18 00:02:34 +0100

    :sparkles: Add create lesson function

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Lesson } from './lesson.entity';
+
+@Injectable()
+export class LessonService {
+  constructor(@InjectRepository(Lesson) private lessonR: Repository<Lesson>) {}
+
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
+    const lesson = this.lessonR.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonR.save(lesson);
+  }
+}
