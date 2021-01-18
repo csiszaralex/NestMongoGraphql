338f7c069e06537f5c0d16d0e8b607cf96628c94
Alex <csiszaralex@gmail.com>
2021-01-18 13:25:52 +0100

    :sparkles: Add functions to students

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { createStudentInput } from './create-student.input';
+import Student from './student.entity';
+import { StudentService } from './student.service';
+import { StudentType } from './student.type';
 
-@Resolver()
-export class StudentResolver {}
+@Resolver((of) => StudentType)
+export class StudentResolver {
+  constructor(private studentS: StudentService) {}
+
+  @Query((returns) => StudentType)
+  student(@Args('id') id: string) {
+    return this.studentS.getLesson(id);
+  }
+
+  @Query((returns) => [StudentType])
+  students() {
+    return this.studentS.getStudents();
+  }
+
+  @Mutation((returns) => StudentType)
+  createStudent(
+    @Args('createStudentInput') createStudentInput: createStudentInput,
+  ) {
+    return this.studentS.createStudent(createStudentInput);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { createStudentInput } from './create-student.input';
+import Student from './student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentR: Repository<Student>,
+  ) {}
+
+  async getLesson(id: string): Promise<Student> {
+    return this.studentR.findOne({ id });
+  }
+
+  async getStudents(): Promise<Student[]> {
+    return this.studentR.find();
+  }
+
+  async createStudent(
+    createStudentInput: createStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+
+    const student = this.studentR.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    return this.studentR.save(student);
+  }
+}
